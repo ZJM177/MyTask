94ae09685b92a687eb43a407f34ee20f1492c8f4
pengjikun <374865031@qq.com>
2017-06-18 01:05:12 +0800

    sun 修改 6.17 2

---
+++ src/main/java/com/sun/controller/SampleController.java
@@ -0,0 +1,21 @@
+package com.sun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by pengjikun on 2017/2/15.
+ */
+@Controller
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String hello() {
+        return "Welcome to sun-members Api!";
+    }
+
+
+
+}
