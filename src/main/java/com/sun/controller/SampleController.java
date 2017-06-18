546f922b5ce01eaa13ed036a0c3be0b73a090346
pengjikun <374865031@qq.com>
2017-06-18 17:28:50 +0800

    sun 修改 6.18

--- src/main/java/com/sun/controller/SampleController.java
+++ src/main/java/com/sun/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.sun.controller;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,12 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by pengjikun on 2017/2/15.
  */
 @Controller
+@Log4j
 public class SampleController {
 
     @RequestMapping("/")
     @ResponseBody
     String hello() {
-        return "Welcome to sun-members Api!";
+        return "Hello world!";
     }
 
 
