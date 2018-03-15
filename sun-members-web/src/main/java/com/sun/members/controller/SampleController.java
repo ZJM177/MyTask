25252aa5cbc2845f7e47a39d726ef035a5d78d46
pengjikun <pengjikun@pekon.com>
2018-03-15 21:43:49 +0800

    18.3.15

---
+++ sun-members-web/src/main/java/com/sun/members/controller/SampleController.java
@@ -0,0 +1,33 @@
+package com.sun.members.controller;
+
+import com.sun.members.service.TestService;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by pengjikun on 2017/2/15.
+ */
+@Controller
+@Log4j
+public class SampleController {
+
+    @Autowired
+    private TestService service;
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello world!";
+    }
+
+    @RequestMapping("/get")
+    @ResponseBody
+    public Object get(@RequestParam("id") int id) {
+        return service.get(id);
+    }
+
+}
